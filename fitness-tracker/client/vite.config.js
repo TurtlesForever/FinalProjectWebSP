5cd6461023d57268f944138ab96a7d8cba13f96a
TurtlesForever <162521693+TurtlesForever@users.noreply.github.com>
2025-04-29 23:50:29 +0000

    main

--- fitness-tracker/client/vite.config.js
+++ fitness-tracker/client/vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [vue()],
   build: {
-    outDir: '../server/public', // must match how the backend serves static files
-  },
+    outDir: '../server/public',
+    emptyOutDir: true
+  }
 })
