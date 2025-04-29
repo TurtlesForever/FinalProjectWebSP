c01f726017f3193d6b5f3aa9edac4d6b92109aa3
TurtlesForever <162521693+TurtlesForever@users.noreply.github.com>
2025-04-29 22:35:28 +0000

    main

--- fitness-tracker/server/server.js
+++ fitness-tracker/server/server.js
@@ -2,13 +2,15 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+// Use the port Render expects
 const PORT = process.env.PORT || 10000;
 
-// Make sure this path is correct!
-app.use(express.static(path.join(__dirname, '../client/dist')));
+// Serve the built Vue frontend
+app.use(express.static(path.resolve(__dirname, '../client/dist')));
 
+// Handle client-side routing (important!)
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+  res.sendFile(path.resolve(__dirname, '../client/dist/index.html'));
 });
 
 app.listen(PORT, () => {
