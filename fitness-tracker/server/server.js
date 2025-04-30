30e7223873f5975fcd7b4bed7b889ce42f43e073
TurtlesForever <162521693+TurtlesForever@users.noreply.github.com>
2025-04-30 03:32:32 +0000

    main

--- fitness-tracker/server/server.js
+++ fitness-tracker/server/server.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const path = require('path');
+
 const app = express();
 const PORT = process.env.PORT || 10000;
 
+// Serve static files from the public directory
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Catch-all route to support client-side routing
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(PORT, () => {
