625132efaa4895dd5afce0d17e2da700591f3d11
TurtlesForever <162521693+TurtlesForever@users.noreply.github.com>
2025-04-30 22:48:26 +0000

    main

--- fitness-tracker/server/models/activityModel.js
+++ fitness-tracker/server/models/activityModel.js
@@ -7,4 +7,4 @@ const userSchema = new mongoose.Schema({
   role:     { type: String, enum: ['user', 'admin'], default: 'user' },
 });
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.models.Activity || mongoose.model('Activity', activitySchema);
