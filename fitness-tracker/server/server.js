54e934dc41cf2fe824f62c0f04696782ccee8763
TurtlesForever <162521693+TurtlesForever@users.noreply.github.com>
2025-04-30 23:32:34 +0000

    main

--- fitness-tracker/server/server.js
+++ fitness-tracker/server/server.js
@@ -8,18 +8,19 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 10000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI, {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
+  useUnifiedTopology: true
 })
 .then(() => console.log('MongoDB connected'))
-.catch((err) => console.error(err));
+.catch((err) => console.error('MongoDB connection error:', err));
 
 // Routes
 const userRoutes = require('./routes/userRoutes');
