52f2a08509402a9cad1192697892f762c8cb9843
TurtlesForever <162521693+TurtlesForever@users.noreply.github.com>
2025-04-30 03:09:03 +0000

    main

--- fitness-tracker/client/vite.config.js
+++ fitness-tracker/client/vite.config.js
@@ -3,16 +3,18 @@ import vue from '@vitejs/plugin-vue';
 import path from 'path';
 
 export default defineConfig({
+  base: '/', // Make sure base is set correctly
+  logLevel: 'info', // Change to 'debug' for even more verbose logs
   plugins: [vue()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'), // Ensure this alias is defined
     },
   },
-  base: '/', // Make sure base is set correctly
   build: {
     outDir: '../server/public', // Make sure the output dir is correct
     emptyOutDir: true,
-    chunkSizeWarningLimit: 2000
+    sourcemap: true, // Enable source maps to debug JS issues
+    chunkSizeWarningLimit: 2000 // Increase the limit if needed
   },
 });
