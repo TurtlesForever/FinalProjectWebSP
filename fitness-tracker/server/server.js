71dd41959013f19bc051a13c9fa7f9f875a84d25
TurtlesForever <162521693+TurtlesForever@users.noreply.github.com>
2025-05-01 02:46:17 +0000

    main

--- fitness-tracker/server/server.js
+++ fitness-tracker/server/server.js
@@ -1,18 +1,31 @@
 const express = require('express');
 const app = express();
-const activityRoutes = require('./routes/activityRoutes');  // Only declare once
 
-// Use the route once
-app.use('/activities', activityRoutes);  // Make sure to use the route here
+// Serve static files from the 'public' directory
+app.use(express.static('public'));
 
-// Other routes
+// Home route (default route)
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html')); // Or serve your homepage
+});
+
+// Other routes for activities, users, etc.
+const activityRoutes = require('./routes/activityRoutes');
+app.use('/activities', activityRoutes);
+
 const userRoutes = require('./routes/userRoutes');
 app.use('/users', userRoutes);
 
-const exerciseTypeRoutes = require('./routes/exerciseTypeRoutes');
-app.use('/exercisetypes', exerciseTypeRoutes);
-
-// Start the server
+// Starting the server
 app.listen(10000, () => {
   console.log('Server running on port 10000');
+});
+
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
+
+// This will ensure that when someone accesses the root route ("/"),
+// they get the index.html file from the build folder.
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
