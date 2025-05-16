d60822f7d5e7d7f16af97e3c505ab71abc050eff
TurtlesForever <162521693+TurtlesForever@users.noreply.github.com>
2025-05-16 02:58:54 +0000

    main

--- fitness-tracker/server/server.js
+++ fitness-tracker/server/server.js
@@ -34,6 +34,7 @@ mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('MongoDB connection error:', err));
 
-app.listen(10000, () => {
-  console.log('Server running on port 10000');
+const PORT = process.env.PORT || 10000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
